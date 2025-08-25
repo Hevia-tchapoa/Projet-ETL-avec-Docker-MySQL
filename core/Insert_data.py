9ca6f6fda241c634f603be72953b39fb8728fa79
Hevia Tchapoa <fabiolitahev@gmail.com>
2025-08-25 13:35:10 +0200

    Generate fake data for table companies

--- core/Insert_data.py
+++ core/Insert_data.py
@@ -6,11 +6,12 @@ import pandas as pd
 
 #Class insert Users    
 class InsertUsers:
+    """Class to insert users into the database Hevia."""
     def __init__(self, cursor, connection):
         self.cursor = cursor
         self.connection = connection
 
-    def insert_users(self, users_list):
+    def insert_users(self, users_list:tuple) -> bool:
             """Insert users into the users table."""
             try:
                 query = "INSERT INTO users (name, email, role) VALUES (%s, %s, %s)"
@@ -21,3 +22,36 @@ class InsertUsers:
             except mysql.connector.Error as e:
                 print(f"❌ Error inserting user: {e}")
             return False
+    
+#insert fake data into companies table
+class InsertCompanies:
+    """Class to insert companies into the database Hevia."""
+    def __init__(self, cursor, connection):
+        self.cursor = cursor
+        self.connection = connection
+
+    def generate_fake_companies(self, num_companies:int) -> bool:
+        """Generate and insert fake company data into the companies table."""
+        fake = Faker()
+        data = []
+        try:           
+            for _ in range(num_companies):
+                name = fake.company()
+                localisation = fake.city()
+                data.append((name, localisation))
+                print(data)
+                insert_companies = "INSERT INTO companies (name, location) VALUES (%s, %s)"
+                self.cursor.executemany(insert_companies, data)
+                self.cursor._connection.commit()
+            print(f"✅ {num_companies} fake companies generated")
+        except mysql.connector.Error as e:
+            print(f"❌ Error generating fake companies: {e}")
+            return False
+        return True
+
+
+
+
+
+
+
